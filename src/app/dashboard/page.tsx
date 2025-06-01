59c030126e63ad172f8e08d98d8f1169f07cbbf7
MarcioParanhos <marcio.naga@gmail.com>
2025-06-01 01:19:00 -0300

    feat: implementar página do dashboard com exibição de informações do usuário e botão de logout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,22 @@
-export default function Dashboard() {
-  return <div>Dashboard</div>;
-}
+import { headers } from "next/headers";
+
+import { auth } from "@/lib/auth";
+
+import SignOutButton from "./components/sign-out-button";
+
+const DashboardPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <h1>{session?.user?.email}</h1>
+      <h1>{session?.user?.name}</h1>
+      <SignOutButton />
+    </div>
+  );
+};
+
+export default DashboardPage;

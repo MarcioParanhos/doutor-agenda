b40fb8778f5129221b65d4d617c7d93af926095e
MarcioParanhos <marcio.naga@gmail.com>
2025-06-01 23:21:55 -0300

    feat: adicionar layout e estrutura à página de médicos, incluindo cabeçalho e botão para adicionar médico

--- src/app/(protected)/_components/app-sidebar.tsx
+++ src/app/(protected)/_components/app-sidebar.tsx
@@ -11,7 +11,7 @@ import {
 } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import {
@@ -62,6 +62,7 @@ export function AppSidebar() {
   const router = useRouter();
 
   const session = authClient.useSession();
+  const pathname = usePathname();
 
   const handleSignOut = async () => {
     await authClient.signOut({
@@ -90,7 +91,7 @@ export function AppSidebar() {
             <SidebarMenu>
               {items.map((item) => (
                 <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
+                  <SidebarMenuButton asChild isActive={pathname === item.url}>
                     <Link href={item.url}>
                       <item.icon />
                       <span>{item.title}</span>
--- src/app/(protected)/doctors/page.tsx
+++ src/app/(protected)/doctors/page.tsx
@@ -1,3 +1,34 @@
+import { PlusIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  PageActions,
+  PageContainer,
+  PageContent,
+  PageDescription,
+  PageHeader,
+  PageHeaderContent,
+  PageTitle,
+} from "@/components/ui/page-container";
+
 export default function DoctorsPage() {
-  return <div>Doctors</div>;
+  return (
+    <PageContainer>
+      <PageHeader>
+        <PageHeaderContent>
+          <PageTitle>Médicos</PageTitle>
+          <PageDescription>Gerencie os médicos da sua clínica</PageDescription>
+        </PageHeaderContent>
+        <PageActions>
+          <Button>
+            <PlusIcon />
+            Adicionar médico
+          </Button>
+        </PageActions>
+      </PageHeader>
+      <PageContent>
+        <h1>Médicos</h1>
+      </PageContent>
+    </PageContainer>
+  );
 }

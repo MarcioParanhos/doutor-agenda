3d2ef2cc2891b358b91d18602c93bde9cc35ef9a
MarcioParanhos <marcio.naga@gmail.com>
2025-06-05 22:17:46 -0300

    refactor: atualizar a página de pacientes para utilizar novo componente AddPatientButton e corrigir a verificação de sessão do usuário; remover arquivo de colunas da tabela de pacientes

--- src/app/(protected)/patients/page.tsx
+++ src/app/(protected)/patients/page.tsx
@@ -16,24 +16,22 @@ import { db } from "@/db";
 import { patientsTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
 
-import { AddPatientButton } from "./_components";
-import { patientsTableColumns } from "./_components/table-colimns";
+import AddPatientButton from "./_components/add-patient-button";
+import { patientsTableColumns } from "./_components/table-columns";
 
 const PatientsPage = async () => {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
-  if (!session) {
+  if (!session?.user) {
     redirect("/authentication");
   }
   if (!session.user.clinic) {
     redirect("/clinic-form");
   }
-
   const patients = await db.query.patientsTable.findMany({
     where: eq(patientsTable.clinicId, session.user.clinic.id),
   });
-
   return (
     <PageContainer>
       <PageHeader>
